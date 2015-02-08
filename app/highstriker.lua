ade3659a05ce87ffffb819e23e21906fa1a4bea2
Tsion Behailu <tsionbehailu@gmail.com>
2015-02-07 20:14:35 -0800

    setting up outline for demo app

---
+++ app/highstriker.lua
@@ -0,0 +1,42 @@
+LCD = require "lcd"
+
+
+-------------------- Server --------------------
+
+-- Set Up Section
+
+
+-- Detect Start, call Set-up
+
+
+	-- Network Section, Send to other device
+
+
+	-- Receive
+
+
+	-- Compute Score
+		-- Get RGB values (map accel value to RGB value)
+
+	
+	-- Display Results
+
+
+-------------------- Client --------------------
+
+-- Listen for Start from Server
+
+	-- Receive
+
+	-- Clear Memory
+
+	-- Get max accel
+
+		-- Keep track of greatest recorded accel
+
+	-- Send score to Server
+
+
+
+--- Call either server or client main function here
+cord.enter_loop()
