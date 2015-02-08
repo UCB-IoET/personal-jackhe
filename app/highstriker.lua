3be83dd76a33e0f5e22ffd87ac474046a4b6e743
Tsion Behailu <tsionbehailu@gmail.com>
2015-02-08 10:20:26 -0800

    finishing up server side

--- app/highstriker.lua
+++ app/highstriker.lua
@@ -12,56 +12,56 @@ shield = require "starter"
 -- Server Main
 function server_main()
 	-- set up LCD (at I2C)
-	lcd = LCD:new(storm.i2c.EXT, 0x7c, storm.i2c.EXT, 0xc4)
+	lcd_setup()
 	-- set up button (plugged into D6)
 	shield.Button.start()
 	btn1 = Button:new("D6")	
 	-- set up Buzzer (optional) (plugged into D2)
 	-- set up network (on 47771) i.e. set up listening socket
-	server = function() 
-		ssock = storm.net.udpsocket(47771, 
-			function () s_handler(payload,from,port) end) 
-		end
+	--server = function() 
+	ssock = storm.net.udpsocket(47771, s_handler)
+	running = true
 	-- loop
+	while running do
 		-- wait for start button to be pressed
 		print("push button 1")
 		shield.Button.when(1, "FALLING", btn1)
 		-- call storm.net.sendto funciton to send start packet to client
-		storm.net.sendto(csock, msg, "ff02::1", 47772)
+		msg = "-2"
+		storm.net.sendto(C_PORT, msg, "ff02::1", 47772)
 		-- wait until you receive an end packet from the client and close the listening socket -- use a signaling variable
 		-- display the final result (do something fun)
-
+	end
 end
 
 --- call from network handler -- NOT IN MAIN LOOP
 function s_handler(payload, from, port)
 	print (string.format("from %s port %d: %s",from,port,payload))
 	-- check if it is an end packet
+	if payload == -1 then
+		running = false
+		return
 	-- if it is an end packet, flag the end signal variable and return
 	-- else Compute Score (from payload, assuming INT data type)
-	compute_score(payload)	
+	else
+		compute_score(payload)
+	end
 	-- Get RGB values (map accel value to RGB value)
 	-- Display Results (look at demo) (ascii bars & color)
 	-- return
 end
 
 -- what is a minimum accel score??
-function compute_score(result)
-	if result < 10 then -- red
-		lcd:setBackColor(255, 0, 0)
-	end
-	if result > 15 then -- green
-		lcd:setBackColor(0, 255, 0)
-	end
-	lcd:setBackColor(0, 0, 255) -- blue
+function compute_score(x)
+	r = math.max((-510*x)/255 + 255, 0)
+	b = math.min(510*x/255,255)
+	g = math.max(510*x/255 - 255,0)
+	lcd:setBackColor(r,g,b)
+	lcd:init(2, 1)
+    	lcd:writeString("Score: " .. x)
 end
 
 -------------------- Client --------------------
-
-function acc_setup() 
-	ac1 = ACC:new()
-	cord.new(function() ac1:init() end)
-end
 
 -- Client Main
 	-- set up accelerometer
@@ -75,13 +75,6 @@ end
 	-- keep track of the max every 50ms
 	-- the total time for this should be less than 2s
 	-- at 2s send the max back to server
-client = function()
-   --local msg = string.format("0x%04x says count=%d", storm.os.nodeid(), count)
-   print("send:", msg)
-   storm.net.sendto(csock, msg, "ff02::1", 47771)  -- send upd echo to link local all nodes multicast 
-   -- count = count + 1
-end
-
 
 --- Call either server or client main function here
 cord.enter_loop()
