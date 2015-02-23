e70e737a268d3c265cf127852f14f6b944b0a8f3
Jack He <siyuanhe@berkeley.edu>
2015-02-22 19:13:18 -0800

    Give myself a clean workspace

--- README.md
+++
@@ -1,3 +0,0 @@
-# ioet_contrib
-
-Shared contrib code for the IoET course. To add your own code here, please fork this and submit a pull request
--- app/shell.lua
+++
@@ -1,9 +0,0 @@
-require "cord"
-sh = require "stormsh"
-
--- start a coroutine that provides a REPL
-sh.start()
-
--- enter the main event loop. This puts the processor to sleep
--- in between events
-cord.enter_loop()
