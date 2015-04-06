12b8f8a0d5ce497aa193fa5e09f42aba4c0e2271
Tony Wu <tony.wu@berkeley.edu>
2015-04-06 12:01:50 -0700

    intermediate push

--- svcd-agent/svcd-agent.lua
+++ svcd-agent/svcd-agent.lua
@@ -2,9 +2,9 @@ require "cord"
 require "svcd"
 sh = require "stormsh"
 
-service_id = 100
-attr_id = 200
-server_ip = "2001:470:4956:1:1cf:2c0b:f0af:2e86"
+service_id = 0x3030
+attr_id = 0x4040
+server_ip = "2001:470:1f04:5f2::2"
 server_port = "7788"
 
 sock = storm.net.udpsocket(server_port, function() end)
@@ -12,12 +12,13 @@ sock = storm.net.udpsocket(server_port, function() end)
 
 onReceive = function(pay, srcip, srcport) 
 	print("got payload: ", pay, " from ", srcip, " portnum: ", srcport)
-	local handle = cord.await(storm.net.sendto, sock, pay, server_ip, 7788)
-	if handle ~= 1 then
-		print "FAIL"
-	else
-		print "OK"
-	end 
+	-- local handle = cord.await(storm.net.sendto, sock, pay, server_ip, 7788)
+	--if handle ~= 1 then
+	--	print "FAIL"
+	--else
+	--	print "OK"
+	--end 
+	SVCD.notify(0x3050, 0x4050, 'what')
 end
 
 
@@ -25,11 +26,17 @@ SVCD.init("bearcast-agent", function()
 	print('initialized')
 	SVCD.add_service(service_id)
 	SVCD.add_attribute(service_id, attr_id, onReceive)
-	--SVCD.advertise()	
+	--cord.new(function()
+	--	while true do
+	--		SVCD.notify(0x3030, 0x4040, 'what')
+	--		cord.await(storm.os.invokeLater, 3*storm.os.SECOND)
+	--	end
+	--end)
 end)
 
 sendMessage = function()
 	storm.net.sendto(sock, storm.mp.pack("hi"), server_ip, server_port)
+	print('send message')
 end
 
 sh.start()
